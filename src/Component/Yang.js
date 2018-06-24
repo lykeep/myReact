fd9a237699f26af43cce8e99e4ad76d2a8480d69
lykeep <2633728793@qq.com>
2018-06-24 20:08:04 +0800

    test

---
+++ src/Component/Yang.js
@@ -0,0 +1,12 @@
+// import React , {Componrnt} from "react"//以后注意单词拼写，
+import React , {Component} from "react"
+
+export default class Yang extends Component{
+    render(){
+        return(
+            <div>
+                <h1>我很聪明！！</h1>
+            </div>
+        )
+    }
+}
